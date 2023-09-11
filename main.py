530309bfef6a2294577e36f7e318dbea239b2196
xwsa568 <xwsa568@gmail.com>
2023-09-11 19:00:03 +0900

    change variable name

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import cv2
+
+from communication import sendNum
 import numpy as np
 
 from camera import webcam
@@ -10,6 +12,8 @@ camera = webcam.WebcamCamera(1)
 pose = pose_estimator.PoseEstimator()
 img = camera.take_photo()
 
+curData = 1 #initial position
+
 """
 while True:
     key = cv2.waitKey(1) & 0xFF  # 1ms 대기 후 키 이벤트 처리
@@ -17,10 +21,14 @@ while True:
         break  # 'q' 키를 누르면 루프 종료
     cv2.imshow("img", img)
 
+
 cv2.destroyAllWindows()
 """
 
 print("on main.py, type(img) and np.shape(img) : ", type(img), np.shape(img))
 
 pose.load_image(img)
-pose.inference()
+newData = pose.inference()
+
+sendNum(curData - newData)
+newData = curData
