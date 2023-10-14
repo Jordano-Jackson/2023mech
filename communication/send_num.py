93fd1d38387a69dd6e748c71dc23b700d2a4fa52
xwsa568 <xwsa568@gmail.com>
2023-10-14 11:30:45 +0900

    bug fixed

--- communication/send_num.py
+++ communication/send_num.py
@@ -1,7 +1,7 @@
 import bluetooth
 def sendNum(data):
     # ESP32의 Bluetooth 주소 (MAC 주소) 입력
-    esp32_mac_address = "XX:XX:XX:XX:XX:XX"
+    esp32_mac_address = "C4:DE:E2:12:F9:62"
     # 블루투스 소켓 생성
     sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     # ESP32와 연결 시도
@@ -13,13 +13,13 @@ def sendNum(data):
         exit(1)
     # 숫자 전송
     if data >= 0:
-        sock.send(“1\n”)
+        sock.send("1\n")
         sock.send(str(data))
-        sock.send(“\n”)
+        sock.send("\n")
     else:
-        sock.send(“0\n”)
+        sock.send("0\n")
         data = -1 * data
         sock.send(str(data))
-        sock.send(“\n”)
+        sock.send("\n")
     # 소켓 닫기
     sock.close()
